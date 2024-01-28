272ecbee2aca5031f704ab9ba47e185063f0efd0
Devrtex <106234691+devrtex@users.noreply.github.com>
2024-01-28 18:37:48 +0800

    Update palworld-maintenance.sh

--- palworld-maintenance.sh
+++ palworld-maintenance.sh
@@ -11,7 +11,7 @@ remote_dir=""
 
 # Create a new backup with timestamp
 backup_file="/home/steam/Palworld_backups/Palworld_$(date '+%Y-%m-%d_%H-%M-%S').tar.gz"
-tar -czvf "$backup_file" /home/steam/.steam/steam/SteamApps/common/PalServer/Pal/Saved
+tar -czvf "$backup_file" /home/steam/.steam/steam/steamapps/common/PalServer/Pal/Saved
 
 # Upload the backup file to FTP server using ncftpput
 ncftpput -u "$ftp_username" -p "$ftp_password" "$ftp_server" "$remote_dir" "$backup_file"
